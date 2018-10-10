abbec95ecb1c302b0d5c2e6af6b58f5022d33d3b
alex <aleksnx@gmail.com>
2018-10-10 23:10:44 +0300

    update win condition

--- code.py
+++ code.py
@@ -18,17 +18,20 @@ print(rules)
 player = str(input('Сделайте свой выбор: %s ' % options))
 comp = str(input('Сделайте выбор за компьютер: %s ' % options))
 
-if player == comp:
-    print('Ничья')
-elif player == options[0] and comp == options[1]:
-    print('Вы выиграли')
-elif player == options[1] and comp == options[2]:
-    print('Вы выиграли')
-elif player == options[2] and comp == options[0]:
-    print('Вы выиграли')
-elif comp == options[0] and player == options[1]:
-    print('Компьютер выиграл')
-elif comp == options[1] and player == options[2]:
-    print('Компьютер выиграл')
-elif comp == options[2] and player == options[0]:
-    print('Компьютер выиграл')
+if (player == options[0] or player == options[1] or player == options[2]) and (comp == options[0] or comp == options[1] or comp == options[2]):
+    if player == comp:
+        print('Ничья')
+    elif player == options[0] and comp == options[1]:
+        print('Вы выиграли')
+    elif player == options[1] and comp == options[2]:
+        print('Вы выиграли')
+    elif player == options[2] and comp == options[0]:
+        print('Вы выиграли')
+    elif comp == options[0] and player == options[1]:
+        print('Компьютер выиграл')
+    elif comp == options[1] and player == options[2]:
+        print('Компьютер выиграл')
+    elif comp == options[2] and player == options[0]:
+        print('Компьютер выиграл')
+else:
+    print('Ваша задача выбрать один из трех предметов «Камень», «Ножницы» или «Бумага».')
